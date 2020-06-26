df6dec38fb9a14e71c78cbeeaa39eee57a6a32dc
kalebgarrison <kalebgarrison@gmail.com>
2020-06-25 23:16:36 -0400

    hard coded readme template and added script to dynamically inject info based on user response.

---
+++ test.md
@@ -0,0 +1,50 @@
+# test
+![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+
+  ## Description
+  test
+
+
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+  test
+
+
+  ## Usage
+  tes
+
+
+  ## License
+  
+
+
+  ## Contributing
+  test
+
+
+  ## Tests
+  test
+
+
+  ## Questions
+
+  test
+
+  If you have any questions please reach out to me here at: test
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,61 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const {title, projectDescription, userInstructions, command, testCommand, licenseChoices, email, username, userContribution } = data
+  return `# ${title}
 
+  ![License](${licenseChoices})
+
+
+  ## Description
+  ${projectDescription}
+
+
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+  ${command}
+
+
+  ## Usage
+  ${userInstructions}
+
+
+  ## License
+  
+
+
+  ## Contributing
+  ${userContribution}
+
+
+  ## Tests
+  ${testCommand}
+
+
+  ## Questions
+
+  ${username}
+
+  If you have any questions please reach out to me here at: ${email}
 `;
+
 }
 
 module.exports = generateMarkdown;
+
+// console.log(data)
